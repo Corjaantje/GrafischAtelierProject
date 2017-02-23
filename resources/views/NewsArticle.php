aaf57dba67ff0ed54e7a370f30f58b1dc9c15123
Rick <rawouter@student.avans.nl>
2017-02-23 12:42:30 +0100

    added new image, replaced and renamed old image

--- resources/views/NewsArticle.php
+++ resources/views/NewsArticle.php
@@ -3,30 +3,34 @@
 
 	<body>
 	
-	<div class="container">
-		<div class="row">
+		<div class="container">
+			<div class="row">
 		
-			<div class="col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
 			
-				<h3>Tentoonstelling GA Grafiekkalender I</h3>
+					<h3>Tentoonstelling GA Grafiekkalender I</h3>
 			
-				<p>Ook voor 2017 maakte Grafisch Atelier Den Bosch een exclsuieve grafiekkalender. Dit keer kan de koper zelf een kalender van 12 samenstellen uit een aanbod van 18 verschillende prenten. In de loop van het jaar zullen we drie tentoonstellingen maken met telkens zes deelnemers aan de kalender. Tijdens de eerste editie zal werk te zien zijn van Martin Copier, Willemijn van Dorp, Peter Koene, Judith Rosema, Marie-Louise Saanen en Karin Schreppers.</p>
-				<p>Wij nodigen u van harte uit aanwezig te zijn op de opening op <strong>zaterdag 4 maart om 16.00 uur.</strong></p>
-				<p>Er zijn nog steeds enkele exemplaren van de kalender te koop. Bestellen kan bij het Grafische Atelier of <a href="http://www.gadenbosch.nl/shop.php">online op de website.</a></p>
+					<p>Ook voor 2017 maakte Grafisch Atelier Den Bosch een exclsuieve grafiekkalender. Dit keer kan de koper zelf een kalender van 12 samenstellen uit een aanbod van 18 verschillende prenten. In de loop van het jaar zullen we drie tentoonstellingen maken met telkens zes deelnemers aan de kalender. Tijdens de eerste editie zal werk te zien zijn van Martin Copier, Willemijn van Dorp, Peter Koene, Judith Rosema, Marie-Louise Saanen en Karin Schreppers.</p>
+					<p>Wij nodigen u van harte uit aanwezig te zijn op de opening op <strong>zaterdag 4 maart om 16.00 uur.</strong></p>
+					<p>Er zijn nog steeds enkele exemplaren van de kalender te koop. Bestellen kan bij het Grafische Atelier of <a href="http://www.gadenbosch.nl/shop.php">online op de website.</a></p>
+			
+				</div>
+		
+				<div class="col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+			
+					<img src="../assets/Images/NewsImages/KarinSchreppersFallingLeaf.jpg" style="width: 100%" />
+					<br>
+					Karin Schreppers, Falling Leaf
+					<br>
+				
+					<img src="../assets/Images/NewsImages/PeterKoeneOmhelsDeNacht.jpg" style="width: 100%">
+					<br>
+					Peter Koene, Omhels de nacht
+			
+				</div>
 			
 			</div>
-		
-			<div class="col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
-			
-				<img src="../assets/Images/NewsImage.jpg" style="width: 100%" />
-				<br>
-				Karin Schreppers, Falling Leaf
-			
-			
-			</div>
-			
 		</div>
-	</div>
 	</body>
 
 </html>
