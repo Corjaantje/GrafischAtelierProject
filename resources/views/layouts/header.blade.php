2f12198c967e56364a522e288d16e306e64499ad
Corjaantje <choskam@gmail.com>
2017-02-24 08:35:19 +0100

    Staging the initial header files
    See title

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Corné
- * Date: 23-Feb-17
- * Time: 12:09
- */
+<html>
+    <head>
+        <title>GA Den Bosch - @yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+    @section('navigation')
+        <div id="nav">
+            <img src="img/logo_ga.png" width="250px">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+
+            </ul>
+            <a href="/"><b>Nieuws</b></a> -
+            <a href="/"><b>Winkel</b></a> -
+            <a href="/"><b>Aan De Slag</b></a> -
+            <a href="/"><b>Over Ons</b>
+                <ul>
+                    <li><a href="/"> test</a></li>
+                </ul>
+            </a>
+        </div>
+    @show
+    <div class="container">
+        @yield('content')
+    </div>
+    </body>
+</html>
