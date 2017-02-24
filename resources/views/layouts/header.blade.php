949d91c26b8db39f6188a4258141ce3e06ddbd1c
Corjaantje <choskam@gmail.com>
2017-02-24 10:19:12 +0100

    Header dropdown
    Created a dropdown menu in the navigation

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,26 +6,21 @@
     <body>
     @section('navigation')
         <div id="nav">
-            <img src="img/logo_ga.png" width="250px">
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-
+                <li> <a href="/" id="logo" > <img src="img/logo_ga.png" width="250px"> </a> </li>
+                <li> <a href="/"> <b>Nieuws</b> </a> </li> -
+                <li> <a href="/"> <b>Winkel</b> </a> </li> -
+                <li> <a href="/"> <b>Aan De Slag</b> </a>
+                    <div id="dropdown-content">
+                        <a href="/"><b>Workshops</b></a>
+                        <a href="/"><b>Scholen</b></a>
+                        <a href="/"><b>Dagje uit</b></a>
+                        <a href="/"><b>Opfrissen</b></a>
+                    </div>
+                </li> -
+                <li> <a href="/"> <b>Over Ons</b> </a> </li>
             </ul>
-            <a href="/"><b>Nieuws</b></a> -
-            <a href="/"><b>Winkel</b></a> -
-            <a href="/"><b>Aan De Slag</b></a> -
-            <a href="/"><b>Over Ons</b>
-                <ul>
-                    <li><a href="/"> test</a></li>
-                </ul>
-            </a>
         </div>
     @show
-    <div class="container">
-        @yield('content')
-    </div>
     </body>
 </html>
