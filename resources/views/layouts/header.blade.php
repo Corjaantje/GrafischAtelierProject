9e191bcb0145de2beac1e7f59177c3a516e9b0f2
Corjaantje <choskam@gmail.com>
2017-03-01 13:49:26 +0100

    Dropdown changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,7 +10,7 @@
                 <li> <a href="/" id="logo" > <img src="img/logo_ga.png" width="250px"> </a> </li>
                 <li> <a href="/"> <b>Nieuws</b> </a> </li> -
                 <li> <a href="/"> <b>Winkel</b> </a> </li> -
-                <li> <a href="/"> <b>Aan De Slag</b> </a>
+                <li id="dropdown"> <a href="/"> <b>Aan De Slag</b> </a>
                     <div id="dropdown-content">
                         <a href="/"><b>Workshops</b></a>
                         <a href="/"><b>Scholen</b></a>
