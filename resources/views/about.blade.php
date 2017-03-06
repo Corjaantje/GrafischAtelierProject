6a9c8468e8d27da2280c25594fbb0d07897b2974
MIDAS-LAPTOP\Midas <midasbroeders@hotmail.com>
2017-03-06 16:01:26 +0100

    Content added
    Used static content from current website, having some trouble with designing the webpage. Currently using a standard bootstrap.css instead of a personalized style.css

---
+++ resources/views/about.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Over GA Den Bosch</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	</head>
+	<body>
+		<div class="jumbotron text-center">
+			<h1>Over GA</h1>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h1>Contact en Info</h1>
+					<p>
+						<b>Kristel van Genugten</b> - Coördinator
+                        <br>
+						<b>Peter Koene</b> - Algemeen werkplaatsbeheer
+						<br>
+                        <b>Maartje van der Kruijs</b> - Beheer zeefdrukafdeling
+						<br>
+                        <b>Nico Thöne</b> - Projectmedewerker Educatie
+                        <br>
+					</p>
+
+
+						<h3>
+							Bestuursleden
+						</h3>
+                    <p>
+						<ul>
+							<li>Jan Ramaekers – voorzitter</li>
+							<li>Hans Koppens – penningmeester</li>
+							<li>Gerard Pels – bestuurslid</li>
+							<li>Roos Terra – bestuurslid</li>
+							<li>Peter Korsman – bestuurslid</li>
+							<li>Hans Derks – bestuurslid</li>
+						</ul>
+					</p>
+
+                    <p>
+                    <h3>
+                        Doelstellingen Grafisch Atelier Den Bosch:
+                    </h3>
+                    <ol>
+                        <li>Het bieden van grafische werkplaatsfaciliteiten voor kunstenaars</li>
+                        <li>Educatie en kennisoverdracht</li>
+                        <li>Aanbieden van exposities en projecten met een relatie tot grafische kunst en vormgeving</li>
+                    </ol>
+                    </p>
+
+                    <a href="http://gadenbosch.nl/pdfs/jaarverslag2015DEF.pdf">Jaarverslag 2015</a>
+				</div>
+
+				<div class="col-md-6">
+
+
+                    <p>
+                    <h1>
+                        Adresgegevens
+                    </h1>
+                    Boschveldweg 471A<br>
+                    5211 VK ‘s-Hertogenbosch<br>
+                    T +31 (0)736 134 277<br>
+                    <a href="mailto:info@gadenbosch.nl">info@gadenbosch.nl</a><br>
+
+                    <ul>
+                        <li>IBAN: NL21INGB0005275472</li>
+                        <li>BIC: INGBNL2A</li>
+                        <li>RSIN/Fiscaal nummer: 0082 31 424</li>
+                        <li>Kamer van Koophandel nummer: 41082086 0000</li>
+                    </ul>
+                    </p>
+					<h3>Openingstijden</h3>
+					<p>
+						ma t/m vrij van 9:00 - 17:00<br>
+						za van 13:00 - 17:00<br>
+					</p>
+					<h3>
+						GA Den Bosch Gesloten:
+					</h3>
+					<p>Op 27 en 28 februari 2017, tijdens carnaval, zijn wij gesloten.</p>
+				</div>
+			</div>
+		</div>
+
+	<!-- 	Voor het GoogleMapsding onderaan de pagina
+			<div id="map" style="width:400px;height:400px;background:yellow"></div>
+
+			<script type="text/javascript">
+				function myMap(){
+					var mapCanvas = document.getElementById("map");
+					var mapOptions = {
+						center: new google.maps.LatLng(51.69545, 5.29674),
+						zoom: 10,
+						mapTypeId: google.maps.MapTypeId.ROADMAP
+					}
+					var map = new google.maps.Map(mapCanvas, mapOptions)
+					var marker = new google.maps.marker({
+						position: google.maps.LatLng(51.69545, 5.29674),
+						map: map,
+						title:"Grafisch Atelier Den Bosch"
+					})
+				}
+			</script>
+
+			<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
+			<! w3 schools key = 'AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU' > -->
+
+	</body>
+</html>
