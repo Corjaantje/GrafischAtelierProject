3d117e144427cb1f1cac954a1aa2ee045db6e51d
MIDAS-LAPTOP\Midas <midasbroeders@hotmail.com>
2017-03-09 17:01:08 +0100

    Logo toegevoegd en commentaar bij archiefartikelen

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -22,10 +22,14 @@
     <div class="content">
         @for($i = 0; $i < 10; $i++)
             <div class="archive-block">
-                <img class="archive-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1200px-Smiley.svg.png" height="150" width="150">
+                <img class="archive-image" src="img/logo_ga_house.png" height="150" width="150">
                 <div class="archive-text">
                     <br>
+                    <!-- Deze tekst moet geteld worden op aantal karakters zodat dit niet te veel wordt, indien te groot -> afsluiten met "..."-->
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br>
+                    <!-- Deze link moet gekoppeld worden aan het desbetreffende product, waarschijnlijk op basis van id waarover ook geloopt wordt-->
+                    <a class="view-more" href="/archive">Bekijk artikel</a>
                 </div>
             </div>
         @endfor
