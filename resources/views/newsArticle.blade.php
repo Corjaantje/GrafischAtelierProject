b55d4476f3c7bd98204e9f6c2cc57edac25d198d
MIDAS-LAPTOP\Midas <midasbroeders@hotmail.com>
2017-04-06 11:18:14 +0100

    Artikelgegevens worden in artikel/{ID} ingeladen

--- resources/views/newsArticle.blade.php
+++ resources/views/newsArticle.blade.php
@@ -20,6 +20,28 @@ use App\NewsData;
 					
 					$itemNr = intval($Id);
 
+					$article = App\NewsArticle::where('Id', '=', $Id)->first();
+
+                    echo "<div class=\"col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1\">";
+
+                    echo "<h1>";
+                    echo $article->Title;
+					echo "</h1>";
+
+					echo "<p> <br> <strong>";
+                    echo $article->Description;
+                    echo "</strong> <br> </p>";
+
+                    echo "<p> <br>";
+                    echo $article->Text;
+                    echo "</p>";
+
+                    echo "</div>";
+
+
+
+
+
 					require '../app/NewsData.php';
 					require '../app/NewsItem.php';
 
@@ -27,61 +49,61 @@ use App\NewsData;
 					$newsItem = $newsData->getNewsItem($itemNr);
 
 					if ($newsItem !== null) {
-		
+
 						$text = $newsItem->getText();
-	
+
 						echo "<div class=\"col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1\">";
-	
+
 						echo $text;
-	
+
 						echo "</div>";
-		
+
 						echo "<div class=\"col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1\">";
-	
+
 						$paths = $newsItem->getPaths();
 						$titles = $newsItem->getPictureName();
-	
+
 						for($i = 0; $i < 99; $i ++) {
-		
+
 							if (isset($paths[$i])) {
 								$pictureName = $paths[$i];
-			
+
 								echo "<img src=\"../img/NewsImages/$pictureName\" style=\"width: 100%;\">";
-			
+
 								if (isset($titles[$i])) {
-				
+
 									$title = $titles[$i];
-				
+
 									echo "<br>";
-				
+
 									echo $title;
 								}
-								
+
 							} else {
 								break;
 							}
 						}
-				
-	
+
+
 						echo "</div>";
-					
+
 					} else {
-		
+
 						echo "<div class=\"col-lg-12 col-md-12 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-1\">";
-	
+
 						echo "<h1>Dit item bestaat niet (meer)</h1>";
-	
+
 						echo "</div>";
 					}
 				} else {
-					
+
 					echo "<div class=\"col-lg-12 col-md-12 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-1\">";
-					
+
 					echo "<h1>Oops something went wrong</h1>";
-					
+
 					echo "</div>";
 				}
-				
+
 			?>
 		</div>
 
