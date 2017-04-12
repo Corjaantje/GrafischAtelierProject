1e518ca7771bce47c0a7661dff06243a1d11163e
Dennis Tijbosch <dtijbosch@student.avans.nl>
2017-04-12 23:16:40 +0200

    added table and techniques seeders

--- database/migrations/2017_04_12_124107_create_individual_reservation_table.php
+++ database/migrations/2017_04_12_124107_create_individual_reservation_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIndividualReservation extends Migration
+class CreateIndividualReservationTable extends Migration
 {
     /**
      * Run the migrations.
