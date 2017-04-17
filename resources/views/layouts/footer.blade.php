ceacaf5c6838a3a554e4d3e9de5427f081710a93
erazorv4 <joepboons@gmail.com>
2017-04-17 15:23:35 +0200

    made changes to footer images

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,12 +7,11 @@
                 <li><a href="/"><b>Vind Ons</b></a></li>
             </ul>
         </div>
-            <div id="footer-links" class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
-
-            </div>
-        <div id="footerimages" class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
-            <img src="{{ URL::asset('img/w2f_logo.png') }}" alt="w2f logo" width="250px">
-            <img src="{{ URL::asset('img/payments.png') }}" alt="payments" width="225px">
+        <div id="footer-images" class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+            <img style="margin-left: 30%" src="{{ URL::asset('img/w2f_logo.png') }}" alt="w2f logo" width="250px">
+        </div>
+        <div id="footer-images" class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+            <img style="margin-left: 30%" src="{{ URL::asset('img/payments.png') }}" alt="payments" width="225px">
         </div>
     </footer>
 @show
