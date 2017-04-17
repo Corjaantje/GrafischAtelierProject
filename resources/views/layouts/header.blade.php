035857cc446eb2e16c32ae86740e55eca1d85a3e
Corjaantje <choskam@gmail.com>
2017-04-17 14:18:14 +0200

    Logout and admin account

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -88,7 +88,7 @@
                                         <a href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Uitloggen
                                         </a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                               style="display: none;">
