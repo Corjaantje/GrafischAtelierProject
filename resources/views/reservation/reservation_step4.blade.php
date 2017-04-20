59d3fd1342fad073df281c13675c15d069fb354f
Dennis Tijbosch <dtijbosch@student.avans.nl>
2017-04-20 14:32:29 +0200

    added default date as today

--- resources/views/reservation/reservation_step4.blade.php
+++ resources/views/reservation/reservation_step4.blade.php
@@ -12,12 +12,12 @@
 @include('layouts.header', array('title'=>'Home'))
 <div class="container">
     <h1 class="title reservationTitles">Voor wanneer wilt u reserveren?</h1>
-    <div class="row">
+    <div class="row"><br /><br />
         <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-4">
             <img src="../../../public/img/Reservation/imgTemp1.jpg" class="reservationImages">
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-4">
-            <b>Datum:</b><br /><input type="date"><br /><br />
+            <b>Datum:</b><br /><input type="date" value="@php echo date("Y-m-d"); @endphp"><br /><br />
             <b>Tijd:</b><br /><input type="time" value="12:00"><br /><br />
             <input type="submit" value="Naar volgende stap" class="btn btn-primary">
         </div>
