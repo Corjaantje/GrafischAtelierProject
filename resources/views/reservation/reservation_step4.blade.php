0a107274b8d3d7a166336d6e03e02208478d53f7
Midas <midasbroeders@hotmail.com>
2017-05-01 14:05:16 +0200

    Stap 3 & 4 fixes.
    Code refactor, images zichtbaar bij stap 4, gekozen tafel zichtbaar op stap 4

--- resources/views/reservation/reservation_step4.blade.php
+++ resources/views/reservation/reservation_step4.blade.php
@@ -10,11 +10,14 @@
 </head>
 <body class="body-cms">
 @include('layouts.header', array('title'=>'Home'))
+
+
 <div class="container">
     <h1 class="title reservationTitles">Voor wanneer wilt u reserveren?</h1>
+    <p id="tafel"></p>
     <div class="row">
         <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-4">
-            <img src="../../../public/img/Reservation/imgTemp1.jpg" class="reservationImages">
+            <img src="{{URL::to('/')}}/img/Reservation/imgTemp1.jpg" class="reservationImages">
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-4">
             <b>Datum:</b><br /><input type="date"><br /><br />
@@ -22,11 +25,15 @@
             <input type="submit" value="Naar volgende stap" class="btn btn-primary">
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-4">
-            <img src="../../../public/img/Reservation/imgTemp2.jpg" class="reservationImages"><br />
-            <img src="../../../public/img/Reservation/imgTemp3.jpg" class="reservationImages">
+            <img src="{{URL::to('/')}}/img/Reservation/imgTemp2.jpg" class="reservationImages"><br />
+            <img src="{{URL::to('/')}}/img/Reservation/imgTemp3.jpg" class="reservationImages">
         </div>
     </div>
 </div>
 @include('layouts.footer')
+
+<script defer>
+    document.getElementById('tafel').innerHTML = 'U heeft ' + sessionStorage.getItem('tafel') + ' geselecteerd';
+</script>
 </body>
 </html>
