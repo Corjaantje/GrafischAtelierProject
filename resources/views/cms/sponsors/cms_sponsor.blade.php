15dc55160916028834a1d11896467b3b47ad87a0
Midas <midasbroeders@hotmail.com>
2017-05-11 09:25:03 +0200

    Helft van sponsoroverzicht

--- resources/views/cms/sponsors/cms_sponsor.blade.php
+++ resources/views/cms/sponsors/cms_sponsor.blade.php
@@ -1,7 +1,7 @@
 @php
     $sponsors = App\Sponsor::all();
 @endphp
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" class="html-cms">
 <head>
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
@@ -13,9 +13,25 @@
 </head>
 <body class="body-cms">
 @if (Auth::check() && Auth::user()->role == "admin")
-
-
 @include('layouts.cms_navigation', array('currentPage'=>'Sponsor Overzicht'))
+<div class="container-cms">
+    <table id="table-style">
+        <tr id="table-row-style">
+            <th id="table-header-style">Logo</th>
+            <th id="table-header-style">Naam</th>
+            <th id="table-header-style">Website</th>
+        </tr>
+        @foreach ($sponsors as $sponsor)
+            <tr id="table-row-style">
+                <td id="table-data-style"><img src="{{URL::asset('/img/Sponsors/'.$sponsor->image)}}"/></td>
+                <td id="table-data-style">{{$sponsor->name}}</td>
+                <td id="table-data-style">{{$sponsor->sponsor_url}}</td>
+            </tr>
+
+        @endforeach
+    </table>
+
+</div>
 @endif
 </body>
 </html>
