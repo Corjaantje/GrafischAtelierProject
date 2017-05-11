ad0b701d964266e1e0bf3d71d7af31821a79cdb0
Corjaantje <choskam@gmail.com>
2017-05-11 15:47:38 +0200

    Revert "Revert "Merge branch 'Hotfix/foreignKeyFix' into develop""
    This reverts commit ef313b4d85dd866366d0a4435a3b51ea4908ed2a.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call('UsersTableSeeder');
         $this->call('NavigationTableSeeder');
         $this->call('WorkshopsTableSeeder');
+        $this->call('NewsfilterTableSeeder');
         $this->call('NewsArticleTableSeeder');
         $this->call('TablesTableSeeder');
         $this->call('CoursesTableSeeder');
