178e3a47663b894cfedc73bebf9400291d0edd6d
Midas <midasbroeders@hotmail.com>
2017-05-15 14:34:06 +0200

    Sponsors worden ingeladen

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,7 @@
 @section('footer')
+    @php
+    $sponsors = App\Sponsor::all();
+    @endphp
     <footer id="footer">
         <div id="footer-links" class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
             <ul>
@@ -7,9 +10,11 @@
                 <li><a href="/"><b>Vind Ons</b></a></li>
             </ul>
         </div>
+        @foreach ($sponsors as $sponsor)
         <div id="footer-images" class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
-            <img style="margin-left: 30%" src="{{ URL::asset('img/w2f_logo.png') }}" alt="w2f logo" width="250px">
+            <a href="{{$sponsor->sponsor_url}}"><img style="margin-left: 30%" src="{{ URL::asset('img/Sponsors/'.$sponsor->image) }}" height="100px" width="100px"></a>
         </div>
+        @endforeach
         <div id="footer-images" class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
             <img style="margin-left: 30%" src="{{ URL::asset('img/payments.png') }}" alt="payments" width="225px">
         </div>
