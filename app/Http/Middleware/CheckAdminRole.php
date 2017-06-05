12507420ffb1e8df45188385bf657130c030cf5f
Rick Wouters <rawouter@student.avans.nl>
2017-06-05 19:05:12 +0200

    Created middleware

---
+++ app/Http/Middleware/CheckAdminRole.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class CheckAdminRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+    	if($request->is('cms/*') || $request->is('cms'))
+    	{
+    		if(Auth::check())
+    		{
+    			if(Auth::user()->role != "admin")
+    			{
+    				return redirect('/home');
+    			}
+    		}
+    		else
+    		{
+    			return redirect('/login');
+    		}
+    	}
+    	
+        return $next($request);
+    }
+}
