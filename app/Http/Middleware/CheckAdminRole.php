8633abea705c63dbd772ae1efe5b3696ac8493d2
Rick Wouters <rawouter@student.avans.nl>
2017-06-06 19:35:31 +0200

    small middleware changes

--- app/Http/Middleware/CheckAdminRole.php
+++ app/Http/Middleware/CheckAdminRole.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class CheckAdminRole
 {
@@ -20,7 +20,7 @@ class CheckAdminRole
     	{
     		if(Auth::check())
     		{
-    			if(Auth::user()->role != "admin")
+    			if(!(Auth::user()->role == 'admin'))
     			{
     				return redirect('/home');
     			}
