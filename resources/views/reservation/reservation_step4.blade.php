520e64e41c233d19818ce9e99fe73acf4f6c2504
Rick Wouters <rawouter@student.avans.nl>
2017-06-07 18:10:44 +0200

    set min and max value reservation times, min value for end is dynamic and both are required

--- resources/views/reservation/reservation_step4.blade.php
+++ resources/views/reservation/reservation_step4.blade.php
@@ -25,8 +25,8 @@
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-4">
             <b>Datum:</b><br /><input type="date" name="date" value="@php echo date("Y-m-d"); @endphp"><br /><br />
-            <b>Starttijd:</b><br /><input type="time" name="start_time" value="12:00"><br /><br />
-            <b>Eindtijd:</b><br /><input type="time" name="end_time" value="15:00"><br /><br />
+            <b>Starttijd:</b><br /><input type="time" name="start_time" value="12:00" min="9:00" max="16:59" id="startTime" required><br /><br />
+            <b>Eindtijd:</b><br /><input type="time" name="end_time" value="15:00" min="12:00" max="17:00" id="endTime" required><br /><br />
             <input type="hidden" name ="table_id" id="table_id" value="">
             <input type="submit" name="btnDateTime" value="Naar volgende stap" class="btn btn-primary">
         </div>
@@ -43,6 +43,10 @@
 <script defer>
     document.getElementById('tafel').innerHTML = 'U heeft ' + sessionStorage.getItem('tafel') + ' geselecteerd';
     document.getElementById('table_id').value = sessionStorage.getItem('tafel_id');
+
+    $('#startTime').change(function() {
+		$('#endTime').attr({"min" : $('#startTime').val()});
+    });
 </script>
 </body>
 </html>
