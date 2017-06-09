5c17ebf6760d1e236da54fa76275016f44a5ab60
Dennis Tijbosch <dtijbosch@student.avans.nl>
2017-06-09 18:49:24 +0200

    changed productdetail pages to modal box popups

--- resources/views/webshop.blade.php
+++ resources/views/webshop.blade.php
@@ -20,7 +20,6 @@ use App\ShopItemNames;
 		
 			@php
 
-
 			echo "<div class=\"row\">";
 			
 			$products = App\Product::all();
@@ -28,33 +27,51 @@ use App\ShopItemNames;
 			foreach($products as $product){
 				
 				$productnr = $product->id;
-				
 				$productTitle = $product->name;
-				
+				$productPrice = $product->price;
+				$productDescription = $product->description;
 				$fileName = "img/WebshopImages/Shop$productnr.jpg";
 					
 				echo "<div class=\"col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1\" >";
 				
-				echo "<a href=\"product/$productnr\">";
-				
-				echo "<img src=\"$fileName\" style=\"width: 100%;\">";
+				echo "<img src=\"$fileName\" style=\"width: 100%;\" data-toggle='modal' data-target=\"#$productnr\">";
 				
 				echo "<br>";
 				
-				echo "$productTitle";
-				
-				echo "</a>";
-				
+				echo "<b>Productnaam</b>: $productTitle";
+
+				echo "<br>";
+
+				echo "<b>Prijs</b>: $productPrice";
+
+				echo "<br>";
+
+				echo "<b>Beschrijving</b>: $productDescription";
+
+				echo "<br>";
+
+				echo "<div class='modal fade' id=\"$productnr\" role='dialog'>";
+					echo "<div class='modal-dialog'>";
+						echo "<div class='modal-content'>";
+							echo "<div class='modal-header'>";
+								echo "<button type='button' class='close' data-dismiss='modal'>&times;</button>";
+								echo "<h4 class='modal-title'>\"$productTitle\"</h4>";
+							echo "</div>";
+							echo "<div class='modal-body'>";
+								echo "<img src=\"$fileName\">";
+							echo "</div>";
+							echo "<div class='modal-footer'>";
+								echo "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>";
+							echo "</div>";
+						echo "</div>";
+					echo "</div>";
 				echo "</div>";
-				
-				
+
+				echo "</div>";
 			}
-			
-			
-			
+
 			echo "</div>";
-				
-			
+
 			@endphp
 		
 		</div>
