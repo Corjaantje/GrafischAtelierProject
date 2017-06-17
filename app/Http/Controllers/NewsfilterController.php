9135d1108f999dcd62d5281ff30059c7adf31958
Midas <midasbroeders@hotmail.com>
2017-06-17 16:24:46 +0200

    Duplicate name error fixed

--- app/Http/Controllers/NewsfilterController.php
+++ app/Http/Controllers/NewsfilterController.php
@@ -63,10 +63,14 @@ class NewsfilterController extends Controller
         if (!$this->checkAdmin()) {
             return Redirect::to('403');
         }
-        if ($_POST['name'] != null) {
-            Newsfilter::Insert(
-                ['name' => $_POST['name']]
-            );
+        if ($_POST['name'] != null)
+        {
+            if (!(Newsfilter::where('name', '=', $_POST['name'])->exists()))
+            {
+                Newsfilter::Insert(
+                    ['name' => $_POST['name']]
+                );
+            }
             return Redirect::to("cms/nieuwsfilters");
         }
     }
@@ -76,10 +80,14 @@ class NewsfilterController extends Controller
         if (!$this->checkAdmin()) {
             return Redirect::to('403');
         }
-        if ($_POST['name'] != null && $_POST['id'] != null) {
-            $filter = Newsfilter::find($_POST['id']);
-            $filter->name = $_POST['name'];
-            $filter->save();
+        if ($_POST['name'] != null && $_POST['id'] != null)
+        {
+            if (!(Newsfilter::where('name', '=', $_POST['name'])->exists()))
+            {
+                $filter = Newsfilter::find($_POST['id']);
+                $filter->name = $_POST['name'];
+                $filter->save();
+            }
             return Redirect::to("cms/nieuwsfilters");
         }
     }
